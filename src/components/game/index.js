61d7f1cff543ad44271435c17585d39013bb6abc
Jwoliv <bohdanlishchuk0@gmail.com>
2024-03-17 23:53:34 +0200

    refactor

--- src/components/game/index.js
+++ src/components/game/index.js
@@ -1,1 +1,6 @@
 export { default as Game } from './Game'
+export { default as GameHeader } from './game-header/GameHeader'
+export { default as GameProfileBlock } from './game-header/GameHeaderProfileBlock'
+export { default as GameHeaderTimer } from './game-header/GameHeaderTimer'
+export { default as GameHeaderTimerBlock } from './game-header/GameHeaderTimerBlock'
+export { default as GameHeaderProfileItem } from './game-header/GameHeaderProfileItem'
