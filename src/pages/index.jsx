083641c47453b4924b60bbaa45204988fd80c85c
Jwoliv <bohdanlishchuk0@gmail.com>
2024-03-17 23:24:55 +0200

    Changed index.js in tje components

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,6 @@
-import Footer from '@/components/footer'
+import { Footer } from '@/components/footer'
 import { Game } from '@/components/game'
-import Header from '@/components/header'
+import { Header } from '@/components/header'
 
 export default function Home() {
     return (
