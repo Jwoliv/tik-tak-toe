913f9f1ee6d9f922112fefc241d0635c8191fc17
Jwoliv <bohdanlishchuk0@gmail.com>
2024-03-20 01:01:10 +0200

    Added margin

--- src/components/game/game-header/GameHeaderProfileItem.jsx
+++ src/components/game/game-header/GameHeaderProfileItem.jsx
@@ -3,8 +3,8 @@ import { GameHeaderTimerBlock } from '..'
 
 const GameHeaderProfileItem = ({ player, isRight }) => {
     return (
-        <div className="relative">
-            <div className={'flex items-center relative justify-between '}>
+        <div className="relative mt-3">
+            <div className={'flex items-center justify-between '}>
                 <div className={('flex relative', !isRight && 'order-1')}>
                     <UserStepIcon style={isRight ? 'absolute right-1 -top-1' : 'absolute left-1 -top-1'} icon={player.symbol} width={9} />
                     <ProfileBlock player={player} />
