72744ddbda80a2c7b3d89df47a7da246895dc374
Jwoliv <bohdanlishchuk0@gmail.com>
2024-03-19 23:36:28 +0200

    Change dynamic

--- src/components/game/game-header/GameHeaderProfileBlock.jsx
+++ src/components/game/game-header/GameHeaderProfileBlock.jsx
@@ -1,5 +1,6 @@
 import { GameHeaderTimerBlock, GameHeaderProfileItem } from '..'
 import GameHeaderProfileRow from './GameHeaderProfileRow'
+import { ICONS_PROGRESS } from '../game-board/constants'
 import cross from './img/cross-icon.svg'
 import zero from './img/zero-icon.svg'
 
@@ -25,8 +26,8 @@ const GameHeaderProfileBlock = () => {
 
     return (
         <div>
-            <GameHeaderProfileRow players={players.slice(0, 2)} icon1={cross} icon2={zero} />
-            <GameHeaderProfileRow players={players.slice(2)} icon1={cross} icon2={zero} />
+            <GameHeaderProfileRow players={players.slice(0, 2)} icon1={ICONS_PROGRESS['cross']} icon2={ICONS_PROGRESS['zero']} />
+            <GameHeaderProfileRow players={players.slice(2)} icon1={ICONS_PROGRESS['tringle']} icon2={ICONS_PROGRESS['square']} />
         </div>
     )
 }
