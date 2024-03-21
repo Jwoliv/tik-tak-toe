c06bbecef6c0a1bdf28c1f63a56f9172f5b4030a
Jwoliv <bohdanlishchuk0@gmail.com>
2024-03-21 23:35:44 +0200

    Added functional of the win

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -3,7 +3,7 @@ import { GameHeader, GameBoard } from '.'
 import { GAME_SYMBOLS } from './game-board/constants'
 
 const Game = () => {
-    const [playersCount, setPlayersCount] = useState(4)
+    const [playersCount, setPlayersCount] = useState(2)
     const [gameState, setGameState] = useState(() => ({
         cells: new Array(19 * 19).fill(null),
         currentProgress: GAME_SYMBOLS.CROSS,
--- src/components/game/game-board/GameBoard.jsx
+++ src/components/game/game-board/GameBoard.jsx
@@ -1,18 +1,15 @@
 import GameHeaderBoard from './GameHeaderBoard'
 import GameBoardSymbol from './GameBoardSymbol'
 import { ICONS_PROGRESS, MOVE_ORDER } from './constants'
+import useWinnerCalculator from '../../../hook/useWinnerSituation'
 
 const GameBoard = ({ playersCount, gameState, setGameState }) => {
     const ORDER = MOVE_ORDER.slice(0, playersCount)
+    const computeWinner = useWinnerCalculator(gameState)
+    const nextProgress = ICONS_PROGRESS[getNextProgress(gameState.currentProgress)]
+    const winnerIndexes = computeWinner(gameState)
 
-    const getNextProgress = (currentProgress) => {
-        let nextIndex = ORDER.indexOf(currentProgress) + 1
-        return ORDER[nextIndex] ?? ORDER[0]
-    }
-
-    const nextProgress = getNextProgress(gameState.currentProgress)
-
-    const clickOnCell = (index) => {
+    function clickOnCell(index) {
         setGameState((lastGameState) => ({
             ...lastGameState,
             cells: updateCells(lastGameState, index),
@@ -20,23 +17,30 @@ const GameBoard = ({ playersCount, gameState, setGameState }) => {
         }))
     }
 
-    const updateCells = (lastGameState, index) => {
+    function updateCells(lastGameState, index) {
         return lastGameState.cells[index] === null
             ? lastGameState.cells.map((cell, idx) => (idx === index ? ICONS_PROGRESS[lastGameState.currentProgress] : cell))
             : lastGameState.cells
     }
 
-    const updateCurrentProgress = (lastGameState, index) => {
+    function updateCurrentProgress(lastGameState, index) {
         return lastGameState.cells[index] === null ? getNextProgress(lastGameState.currentProgress) : lastGameState.currentProgress
     }
 
+    function getNextProgress(currentProgress) {
+        let nextIndex = ORDER.indexOf(currentProgress) + 1
+        return ORDER[nextIndex] ?? ORDER[0]
+    }
+
+    console.log(computeWinner())
+
     return (
         <div>
-            <GameHeaderBoard currentProgress={ICONS_PROGRESS[gameState.currentProgress]} nextProgress={ICONS_PROGRESS[nextProgress]} />
+            <GameHeaderBoard currentProgress={ICONS_PROGRESS[gameState.currentProgress]} nextProgress={nextProgress} />
             <div className="grid grid-cols-[repeat(19,_30px)] grid-rows-[repeat(19,_30px)] pt-px pl-px mt-4">
                 {gameState.cells.map((symbol, index) => (
                     <GameBoardSymbol
-                        className="shadow-none"
+                        className={`shadow-none ${winnerIndexes && winnerIndexes.includes(index) ? 'winner-cell' : ''}`}
                         width={20}
                         key={index}
                         icon={symbol}
